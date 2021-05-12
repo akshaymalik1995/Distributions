0cfa5fd6e86814059ffc29c045872be681a49850
akshaymalik1995 <akshaymalik191@gmail.com>
2021-05-12 22:19:47 +0530

    Added Geometric

--- poisson-dist.py
+++ poisson-dist.py
@@ -1,46 +1,119 @@
-# import streamlit
-# check if it is working
+
 import streamlit
-from scipy.stats import poisson
+from scipy.stats import poisson, geom
 import seaborn as sns
 import plotly.figure_factory as ff
 import plotly.express as px
 import math
 import pandas as pd
+import plotly.graph_objects as go
+import numpy as np
+from plotly.subplots import make_subplots
+
+distributions = streamlit.sidebar.selectbox(
+    "Select the distribution",
+    ("Poisson Distribution", "Geometric Distribution")
+)
 
 
-mu = streamlit.number_input("Insert the value of mu", step=1.0, min_value=1.0)
-k = streamlit.number_input("Insert the value of K", step=1.0, min_value=0.0)
 
 
 
 
-
-
-if mu and k:
+def poisson_dist(k, mean):
+    mean = mean
     dictionary = {"k" : [] , "pmf" : [], "cdf" : [], "sf" : []}
 
 
-    for i in range(1, 2 * int(mu) + 1):
+    for i in np.arange(1, mean * 2 + 1, 1):
         dictionary["k"].append(i)
-        dictionary['pmf'].append(poisson.pmf(i, mu))
-        dictionary['cdf'].append(poisson.cdf(i, mu))
-        dictionary['sf'].append(poisson.sf(i, mu))
+        dictionary['pmf'].append(poisson.pmf(i, mean))
+        dictionary['cdf'].append(poisson.cdf(i, mean))
+        dictionary['sf'].append(poisson.sf(i, mean))
 
     df = pd.DataFrame(dictionary)
-    def highlight_k(x):
-        if x.k == k:
-            return ['background-color: yellow']*4
-        else:
-            return ['background-color: white'] * 4
+    df = df.round(decimals=4)
+    answers = df.iloc[int(k - 1)]
+    streamlit.write(f"P(X = {answers[0]}) is {answers[1]}")
+    streamlit.write(f"P(X <= {answers[0]}) is {answers[2]}")
+    streamlit.write(f"P(X >= {answers[0]}) is {answers[3]}")
 
-    func = streamlit.radio("Choose the function: ", ("pmf", "cdf", "sf"))
-    fig = px.line(df, x="k", y=func)
-    streamlit.plotly_chart(fig)     
-    
-    df = df.style.apply(highlight_k, axis=1)
+    fig = make_subplots(
+        rows=3, cols=1,
+        subplot_titles=("Probability of X = K", "Probability of X <= K", "Probability of X >= K"))
 
-    streamlit.write(df)
+
+    # fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df['k'], y=df["pmf"], name="P(X = k)",  fill='tozeroy'), row=1, col=1)
+    fig.add_trace(go.Scatter(x=df['k'], y=df["cdf"], name="P(X <= k)", fill='tozeroy'),row=2, col=1)
+    fig.add_trace(go.Scatter(x=df['k'], y=df["sf"], name="P(X > k)", fill='tozeroy'),row=3, col=1)
+
+    fig.update_layout(hovermode="closest", height=1000)
+
+    streamlit.plotly_chart(fig)
+
+   
+def geom_dist(p, k):
+    probability = p
+    size = 30
+
+
+
+    dictionary = {"k" : [] , "pmf" : [], "cdf" : [], "sf" : []}
+
+
+    for i in np.arange(1, size, 1):
+        dictionary["k"].append(i)
+        dictionary['pmf'].append(geom.pmf(i, probability))
+        dictionary['cdf'].append(geom.cdf(i, probability))
+        dictionary['sf'].append(geom.sf(i, probability))
+
+    df = pd.DataFrame(dictionary)
+    df = df.round(decimals=4)
+
+    answers = df.iloc[int(k - 1)]
+    streamlit.write(f"P(G = {answers[0]}) is {answers[1]}")
+    streamlit.write(f"P(G <= {answers[0]}) is {answers[2]}")
+    streamlit.write(f"P(G >= {answers[0]}) is {answers[3]}")
+
+    fig = make_subplots(
+        rows=3, cols=1,
+        subplot_titles=("Probability of G = K", "Probability of G <= K", "Probability of X >= K"))
+
+
+    # fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df['k'], y=df["pmf"], name="P(G = k)",  fill='tozeroy'), row=1, col=1)
+    fig.add_trace(go.Scatter(x=df['k'], y=df["cdf"], name="P(G <= k)", fill='tozeroy'),row=2, col=1)
+    fig.add_trace(go.Scatter(x=df['k'], y=df["sf"], name="P(G > k)", fill='tozeroy'),row=3, col=1)
+    
+
+    fig.update_layout(hovermode="closest", height=1000)
+
+    streamlit.plotly_chart(fig)
+
+    
+   
+if distributions == "Poisson Distribution":
+    streamlit.title("Poisson Distribution")
+    mu = streamlit.sidebar.number_input("Insert the value of mu", step=1.0, min_value=1.0)
+    k = streamlit.sidebar.slider("Insert the value of K", step=1.0, min_value=0.0, max_value=2 * mu)
+    if mu and k:
+        poisson_dist(k, mu)
+if distributions == "Geometric Distribution":
+    streamlit.title("Geometric Distribution")
+    probability = streamlit.sidebar.slider("Insert the probability", step=0.01, min_value=0.0, max_value=1.0)
+    k = streamlit.sidebar.number_input("Insert the value of K", step=1.0, min_value=0.0)
+    if probability and k:
+        geom_dist(probability, k)
+
+
+
+
+
+   
     
     
     
+    
+    
+
