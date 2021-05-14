c734093c0a91bb71d9b47ce881a38d5844782195
akshaymalik1995 <akshaymalik191@gmail.com>
2021-05-14 11:42:49 +0530

    Binomial Distribution Added

--- dist.py
+++ dist.py
@@ -1,6 +1,6 @@
 
 import streamlit
-from scipy.stats import poisson, geom
+from scipy.stats import poisson, geom, binom
 import seaborn as sns
 import plotly.figure_factory as ff
 import plotly.express as px
@@ -10,9 +10,10 @@ import plotly.graph_objects as go
 import numpy as np
 from plotly.subplots import make_subplots
 
+
 distributions = streamlit.sidebar.selectbox(
     "Select the distribution",
-    ("Poisson Distribution", "Geometric Distribution")
+    ("Binomial Distribution", "Poisson Distribution", "Geometric Distribution")
 )
 
 
@@ -25,7 +26,7 @@ def poisson_dist(mean):
     dictionary = {"k" : [] , "pmf" : [], "cdf" : [], "sf" : []}
 
 
-    for i in np.arange(1, mean * 2 + 1, 1):
+    for i in np.arange(0, mean * 2 + 1, 1):
         dictionary["k"].append(i)
         dictionary['pmf'].append(poisson.pmf(i, mean))
         dictionary['cdf'].append(poisson.cdf(i, mean))
@@ -58,7 +59,7 @@ def geom_dist(p):
     probability = p
     size = 30
 
-
+    
 
     dictionary = {"k" : [] , "pmf" : [], "cdf" : [], "sf" : []}
 
@@ -70,7 +71,7 @@ def geom_dist(p):
         dictionary['sf'].append(geom.sf(i, probability))
 
     df = pd.DataFrame(dictionary)
-    df = df.round(decimals=4)
+    # df = df.round(decimals=4)
 
     # answers = df.iloc[int(k - 1)]
     # streamlit.write(f"P(G = {answers[0]}) is {answers[1]}")
@@ -92,6 +93,45 @@ def geom_dist(p):
 
     streamlit.plotly_chart(fig)
 
+
+def binomial_dist(p, n):
+    
+    size = n
+
+    
+
+    dictionary = {"k" : [] , "pmf" : [], "cdf" : [], "sf" : []}
+
+
+    for i in np.arange(0, size + 1, 1):
+        dictionary["k"].append(i)
+        dictionary['pmf'].append(binom.pmf(i, n, p))
+        dictionary['cdf'].append(binom.cdf(i,n , p))
+        dictionary['sf'].append(binom.sf(i,n , p))
+
+    df = pd.DataFrame(dictionary)
+    # df = df.round(decimals=4)
+
+    # answers = df.iloc[int(k - 1)]
+    # streamlit.write(f"P(G = {answers[0]}) is {answers[1]}")
+    # streamlit.write(f"P(G <= {answers[0]}) is {answers[2]}")
+    # streamlit.write(f"P(G > {answers[0]}) is {answers[3]}")
+
+    fig = make_subplots(
+        rows=3, cols=1,
+        subplot_titles=("Probability of X = K", "Probability of X <= K", "Probability of X > K"))
+
+
+    # fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df['k'], y=df["pmf"], name="P(X = k)",  fill='tozeroy'), row=1, col=1)
+    fig.add_trace(go.Scatter(x=df['k'], y=df["cdf"], name="P(X <= k)", fill='tozeroy'),row=2, col=1)
+    fig.add_trace(go.Scatter(x=df['k'], y=df["sf"], name="P(X > k)", fill='tozeroy'),row=3, col=1)
+    
+
+    fig.update_layout(hovermode="closest", height=1000)
+
+    streamlit.plotly_chart(fig)
+
     
    
 if distributions == "Poisson Distribution":
@@ -100,12 +140,20 @@ if distributions == "Poisson Distribution":
     # k = streamlit.sidebar.slider("Insert the value of K", step=1.0, min_value=0.0, max_value=2 * mu)
     if mu:
         poisson_dist(mu)
+
 if distributions == "Geometric Distribution":
     streamlit.title("Geometric Distribution")
-    probability = streamlit.sidebar.slider("Insert the probability", step=0.01, min_value=0.0, max_value=1.0)
+    probability = streamlit.sidebar.number_input("Insert the probability",format="%f" , step=0.001, min_value=0.0, max_value=1.0)
     # k = streamlit.sidebar.number_input("Insert the value of K", step=1.0, min_value=0.0)
     if probability:
         geom_dist(probability)
+
+if distributions == "Binomial Distribution":
+    streamlit.title("Binomial Distribution")
+    probability = streamlit.sidebar.number_input("Insert the probability", format="%f" , step=0.001, min_value=0.0, max_value=1.0)
+    trials = streamlit.sidebar.number_input("Insert the number of trials", step=1.0, min_value=0.0)
+    if probability:
+        binomial_dist(probability, trials)
 
 
 
